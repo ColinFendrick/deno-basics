777091ee1bf07d59ffb4001d34cb6146bca6cfec
Colin Fendrick <colin.fendrick@gmail.com>
2020-11-06 12:30:16 -0800

    Break the story map handler out

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import { serve } from 'https://deno.land/std/http/server.ts';
-import format from 'https://deno.land/x/date_fns/format/index.js';
+
+import { mapStory } from './stories.js';
 
 export default url => {
 	const server = serve({ port: 8000 });
@@ -7,16 +8,9 @@ export default url => {
 
 	(async () => {
 		for await (const req of server) {
-			const result = await fetch(url).then((result) => result.json());
+			const result = await fetch(url).then(result => result.json());
 
-			const stories = result.hits.map((hit) => ({
-				title: hit.title,
-				url: hit.url,
-				createdAt: format(
-					new Date(hit.created_at_i * 1000),
-					'yyyy-MM-dd'
-				)
-			}));
+			const stories = result.hits.map(mapStory);
 
 			req.respond({
 				body: JSON.stringify(stories)
